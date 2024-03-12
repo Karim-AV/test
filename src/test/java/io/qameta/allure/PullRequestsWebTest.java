9e02ce5bad92973b1b9f6db836759b20ff39f5b2
Karim <110749575+Karim-AV@users.noreply.github.com>
2024-03-12 21:34:37 +0600

    Update PullRequestsWebTest.java

--- src/test/java/io/qameta/allure/PullRequestsWebTest.java
+++ src/test/java/io/qameta/allure/PullRequestsWebTest.java
@@ -33,9 +33,7 @@ public class PullRequestsWebTest {
     @Tags({@Tag("web"), @Tag("regress"), @Tag("smoke")})
     @DisplayName("Creating new brilliant request by authorized user")
     public void shouldCreateBrilliantPullRequest() {
-        steps.openBrilliantPullRequestsPage();
-        steps.createBrilliantPullRequestFromBranch();
-        steps.shouldSeeBrilliantPullRequestForBranch();
+        Allure.step("new simple step");
     }
     
     @Test
